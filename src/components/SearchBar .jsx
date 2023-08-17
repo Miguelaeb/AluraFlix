d1325e4c7e3d8dde4b307558c7a15e9f046e616d
miguel evangelista <miguelbranagan30@gmail.com>
2023-08-16 23:46:09 -0400

    starting trending section and json server created

--- src/components/SearchBar .jsx
+++ src/components/SearchBar .jsx
@@ -2,13 +2,19 @@ import PropTypes from "prop-types";
 
 export default function SearchBar({ placeholder }) {
     return (
-        <section className="flex gap-4 my-[1.63rem] mx-4 relative">
-            <img src="/images/Searchicon.svg" alt="Search" className="z-10" />
-            <input
-                className="w-full bg-[#10141E] focus:border-b-[1px] outline-none text-white focus:text-white focus:border-[#5A698F] focus:pb-[0.94rem] caret-[#FC4747] placeholder:opacity-[0.4979]"
-                type="text"
-                placeholder={placeholder}
-            />
+        <section className=" my-[1.63rem] mx-4 h-[30px]">
+            <div className=" flex gap-4">
+                <img
+                    src="/images/Searchicon.svg"
+                    alt="Search"
+                    className="z-10"
+                />
+                <input
+                    className="w-full bg-[#10141E] focus:border-b-[1px] outline-none text-white focus:text-white focus:border-[#5A698F] focus:pb-[0.94rem] caret-[#FC4747] placeholder:opacity-[0.4979]"
+                    type="text"
+                    placeholder={placeholder}
+                />
+            </div>
         </section>
     );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import SearchBar from "../components/SearchBar ";
+import Trending from "../components/Trending";
 
 export default function Home() {
     return (
         <>
             <SearchBar placeholder="Search for movies or TV series" />
+            <Trending />
         </>
     );
 }
