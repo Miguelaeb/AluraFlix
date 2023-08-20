a22bf8ef462c7dde6aa9ef0442ff20c28cea1051
miguel evangelista <miguelbranagan30@gmail.com>
2023-08-19 23:33:18 -0400

    hidding scroll bar

--- src/components/Trending.jsx
+++ src/components/Trending.jsx
@@ -7,7 +7,7 @@ export default function Trending() {
         <section className="ml-4 md:ml-[1.58rem] mb-[2.44rem]">
             <h2 className="sectionTitle">Trending</h2>
 
-            <div className="scroll overflow-x-auto overflow-y-hidden mt-4">
+            <div className="scroll overflow-x-auto overflow-y-hidden mt-4 hide-scrollbar">
                 <div className="grid grid-flow-col gap-10">
                     {content.map((item) => (
                         <div className="w-[15rem] h-full" key={item.id}>
